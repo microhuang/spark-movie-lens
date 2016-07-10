a6e2117fe035021bb9a65cf09814aa5308d86c33
microhuang <yangfan@mogujie.com>
2016-07-11 01:56:12 +0800

    bayes

--- start_server.sh
+++ start_server.sh
@@ -2,6 +2,8 @@ export SPARK_MOVIE_LENS_DIR="file:/tmp"
 
 SPARK_HOME=~/spark-2.0.0-preview-bin-hadoop2.6
 
-SPARK_MASTER=spark://169.254.206.2:7077
+#SPARK_MASTER=spark://169.254.206.2:7077
+SPARK_MASTER=spark://192.168.0.11:7077
+#SPARK_MASTER=yarn
 
 ${SPARK_HOME}/bin/spark-submit --master ${SPARK_MASTER} --deploy-mode client --queue default server.py 
