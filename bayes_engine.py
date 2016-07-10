beca89519ee8cecfcb9c7699590af3ef32526658
microhuang <yangfan@mogujie.com>
2016-07-11 02:11:28 +0800

    bayes

--- bayes_engine.py
+++ bayes_engine.py
@@ -14,12 +14,12 @@ class BayesEngine:
 
     def __init__(self, sc, dataset_path):
         logger.info("Loading Ratings data...")
+        #label token,token,...
         ratings_file_path = os.path.join(dataset_path, 'ratings.csv')
         ratings_raw_RDD = self.sc.textFile(ratings_file_path)
         ratings_raw_data_header = ratings_raw_RDD.take(1)[0]
         self.ratings_RDD = ratings_raw_RDD.filter(lambda line: line!=ratings_raw_data_header)\
             .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),int(tokens[1]),float(tokens[2]))).cache()
-
         if os.path.isfile('/tmp/Bayes.model'):
             self.model = NaiveBayesModel.load(sc, '/tmp/Bayes.model')
         if not self.model:
