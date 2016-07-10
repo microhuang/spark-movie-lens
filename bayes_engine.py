3ba10e1441a26a0fe7a20264e55f915d1c7f7c63
microhuang <yangfan@mogujie.com>
2016-07-11 01:56:28 +0800

    bayes

---
+++ bayes_engine.py
@@ -0,0 +1,27 @@
+from pyspark.mllib.classification import NaiveBayes, NaiveBayesModel
+
+import os
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class BayesEngine:
+
+    def __train_model(self):
+        self.model = NaiveBayes.train(self.ratings_RDD, 1.0)
+        logger.info("Bayes model built!")
+
+    def __init__(self, sc, dataset_path):
+        logger.info("Loading Ratings data...")
+        ratings_file_path = os.path.join(dataset_path, 'ratings.csv')
+        ratings_raw_RDD = self.sc.textFile(ratings_file_path)
+        ratings_raw_data_header = ratings_raw_RDD.take(1)[0]
+        self.ratings_RDD = ratings_raw_RDD.filter(lambda line: line!=ratings_raw_data_header)\
+            .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),int(tokens[1]),float(tokens[2]))).cache()
+
+        if os.path.isfile('/tmp/Bayes.model'):
+            NaiveBayesModel.load(sc, '/tmp/Bayes.model')
+        else:
+            self.__train_model()
+            self.model.save(self.sc,'/tmp/Bayes.model')
+
